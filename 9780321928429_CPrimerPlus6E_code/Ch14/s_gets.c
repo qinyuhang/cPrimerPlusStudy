44a797f2bf12e037c69284eceafb20aeaed256d5
秦宇航 <qinyuhangxiaoxiang@gmail.com>
2017-02-19 00:32:21 +0800

    abstract s_get function into library

---
+++ 9780321928429_CPrimerPlus6E_code/Ch14/s_gets.c
@@ -0,0 +1,19 @@
+#include "s_gets.h"
+
+char * s_gets(char * st, int n)
+{
+    char * ret_val;
+    char * find;
+
+    ret_val = fgets(st, n, stdin);
+    if (ret_val)
+    {
+        find = strchr(st, '\n');   // look for newline
+        if (find)                  // if the address is not NULL,
+            *find = '\0';          // place a null character there
+        else
+            while (getchar() != '\n')
+                continue;          // dispose of rest of line
+    }
+    return ret_val;
+}
---
+++ 9780321928429_CPrimerPlus6E_code/Ch14/s_gets.h
@@ -0,0 +1,3 @@
+#include <string.h>
+#include <stdio.h>
+char * s_gets(char * st, int n);
